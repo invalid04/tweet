7109d5813a8a66c7b2ce63481b1e8a24d1e6ada6
invalid04 <68178371+invalid04@users.noreply.github.com>
2023-07-18 23:35:38 -0500

    Create posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,23 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import Image from "next/image";
 import { LoadingPage }  from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const {user} = useUser();
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   console.log(user);
 
@@ -26,7 +38,15 @@ const CreatePostWizard = () => {
         width={56}
         height={56}
       />
-      <input placeholder='Type something...' className='bg-transparent grow outline-none'/>
+      <input 
+        placeholder='Type something...' 
+        className='bg-transparent grow outline-none'
+        value={input}
+        type='text'
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
+        />
+        <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   )
 }
@@ -49,7 +69,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span>
           <span className='font-thin'>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className='text-2xl'>{post.content}</span>
       </div>
     </div>
   )
@@ -64,7 +84,7 @@ const Feed = () => {
 
   return (
     <div className='flex flex-col'>
-    {[...data, ...data]?.map(( fullPost) => (
+    {data.map(( fullPost) => (
       <PostView {...fullPost} key={fullPost.post.id} />
     ))}
   </div>
